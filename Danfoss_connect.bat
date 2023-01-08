faec66fb9990bda08b9e7ccde259cc3ba40c0f92
AIXruth <norbert.weuster@arcor.de>
2023-01-08 11:49:24 +0100

    change to read,set,read

--- Danfoss_connect.bat
+++ Danfoss_connect.bat
@@ -1,3 +1,9 @@
+node .\Danfoss_connect.js
+echo "done1"
 set /p T="Which temperature do you want (press enter to output current settings)?"
 node .\Danfoss_connect.js %T%
+echo "done2"
+timeout 5
+node .\Danfoss_connect.js
+echo "done3"
 pause
